1c9ac6d35e0f89eb90feb7d52f0a42f921d1d9ff
Stephen Cleary <liveid.20.stephen-cleary@spamgourmet.com>
2015-03-29 19:59:37 -0400

    Completed unit tests.

--- test/UnitTests/AsyncAssertUnitTests.cs
+++ test/UnitTests/AsyncAssertUnitTests.cs
@@ -602,5 +602,45 @@ namespace UnitTests
                 AsyncAssert.Cancels(() => { throw new Exception(); });
             });
         }
+
+        [Fact]
+        public async Task NeverCompletesAsync_TaskCompletes_Fails()
+        {
+            await Assert.ThrowsAsync<Exception>(async () =>
+            {
+                var tcs = new TaskCompletionSource<object>();
+                var testTask = AsyncAssert.NeverCompletesAsync(tcs.Task);
+                tcs.SetResult(null);
+                await testTask;
+            });
+        }
+
+        [Fact]
+        public async Task NeverCompletesAsync_TaskFaults_Fails()
+        {
+            await Assert.ThrowsAsync<Exception>(async () =>
+            {
+                var tcs = new TaskCompletionSource<object>();
+                var testTask = AsyncAssert.NeverCompletesAsync(tcs.Task);
+                tcs.SetException(new Exception());
+                await testTask;
+            });
+        }
+
+        [Fact]
+        public async Task NeverCompletesAsync_SynchronousTaskCompletes_Fails()
+        {
+            await Assert.ThrowsAsync<Exception>(async () =>
+            {
+                await AsyncAssert.NeverCompletesAsync(Task.FromResult(0));
+            });
+        }
+
+        [Fact]
+        public async Task NeverCompletesAsync_NeverCompletes_Passes()
+        {
+            var tcs = new TaskCompletionSource<object>();
+            await AsyncAssert.NeverCompletesAsync(tcs.Task);
+        }
     }
 }
